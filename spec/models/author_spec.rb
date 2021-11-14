912eb48525a5f4cc6e8d14cc5dab712f50e81e04
HoangAn <hoangan.nguyen@studen.hpi.de>
2021-11-14 14:50:43 +0100

    solve #4

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe Author, type: :model do
+describe 'Author model', type: :model do
     it "should have attributes first_name last_name and homepage " do
-      author = Author.new()
-      expect(author.first).to eq('hello')
+      author = Author.new(first_name: "Alan", last_name:"Turing", homepage:"http://wikipedia.org/Alan_Turing")
+      
+      expect(author.first_name).to eq("Alan")
+      expect(author.last_name).to eq("Turing")
+      expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
+    end
+
+    it "should have name method which return first and last name" do
+      author = Author.new(first_name: "Alan", last_name:"Turing", homepage:"http://wikipedia.org/Alan_Turing")
+      expect(author.name()).to eq("Alan Turing") 
+    end
 end
