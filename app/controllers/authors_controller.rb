6ede1dc986437393aa51017a7cf8d48c01a7df4d
HoangAn <hoangan.nguyen@studen.hpi.de>
2021-11-14 17:57:41 +0100

    solve #10

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,11 +1,13 @@
 class AuthorsController < ApplicationController
-    def new
-        @author = Author.new
-    end
-
     def index
         @authors = Author.all
     end 
+    def new
+        @author = Author.new
+    end
+    def edit
+        @author = Author.find(params[:id])
+    end
     def create
         @author = Author.new(author_params)
         if @author.save
@@ -18,6 +20,14 @@ class AuthorsController < ApplicationController
     end 
     def show
         @author = Author.find(params[:id])
+    end
+    def update
+        @author = Author.find(params[:id])
+        if @author.update(author_params)
+            redirect_to root_path, notice: 'Success!'
+        else 
+            render "edit"
+        end
     end
     private    
     def author_params
